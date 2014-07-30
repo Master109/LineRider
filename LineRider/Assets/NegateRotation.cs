f84095a1bbf06c347c6b1e2675cc2e110a5ff8f0
Master109 <gileadcosman@Gileads-MacBook-Pro.local>
2014-07-29 21:41:22 -0700

    Initial commit

---
+++ LineRider/Assets/NegateRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NegateRotation : MonoBehaviour
+{
+	Quaternion initRota;
+
+	// Use this for initialization
+	void Start ()
+	{
+		initRota = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.rotation = initRota;
+	}
+}
---
+++ LineRider/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public int speed;
+	public int jumpForce;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnCollisionStay2D (Collision2D coll)
+	{
+		bool move = false;
+		foreach (ContactPoint2D cp in coll.contacts)
+			if (cp.point.y < transform.position.y)
+				move = true;
+		if (move)
+		{
+			rigidbody2D.AddForce(Vector2.right * Input.GetAxis("X") * speed);
+			if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+				rigidbody2D.AddForce(Vector2.up * jumpForce);
+		}
+	}
+}
